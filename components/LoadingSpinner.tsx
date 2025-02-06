5430cffd4c1ab0d922c58241a27eab0d51e02620
dorik84 <dorya2005@mail.ru>
2025-02-06 19:55:21 -0400

    productCard ~responsivnes

--- components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center w-full h-64">
-      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary"></div>
-    </div>
-  )
-}
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -10,19 +10,20 @@ export default function ProductCard({ product }: { product: ProductData }) {
     <motion.div
       whileHover={{ scale: 1.05 }}
       transition={{ duration: 0.2 }}
-      className="flex-shrink-0 w-64 bg-white rounded-lg shadow-md overflow-hidden"
+      className="flex-shrink-0 w-32 md:w-64 bg-white rounded-lg shadow-custom-shadow overflow-hidden"
     >
       <Link href={`/product/${product.url}`}>
-        <div className="relative h-48">
+        <div className="relative h-24 md:h-48">
           <Image
             src={product.images[0] || "/placeholder.svg"}
             alt={product.title}
             fill
-            style={{ objectFit: "cover" }}
+            style={{ objectFit: "contain" }}
+            className="w-full h-full"
           />
         </div>
         <div className="p-4 truncate">
-          <h3 className="text-lg font-semibold mb-2 truncate">{product.title}</h3>
+          <h3 className="text-sm md:text-lg font-semibold mb-2 truncate">{product.title}</h3>
           <p className="text-gray-600">${product.defaultPrice.toFixed(2)}</p>
         </div>
       </Link>
