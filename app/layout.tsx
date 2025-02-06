7d5e218b9c4b9da4a47c57acbfa69b79c988ffa9
dorik84 <dorya2005@mail.ru>
2025-02-06 19:54:25 -0400

    moved some comp from product to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import AffiliateMessage from "@/components/AffiliateMessage";
+import ThemeToggle from "@/components/ThemeToggle";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,6 +21,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${inter.className} bg-custom-radial bg-custom-size bg-fixed`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="flex justify-between items-center mb-4 px-4 gap-4 sm:mb-6">
+            <AffiliateMessage />
+            <ThemeToggle />
+          </div>
           {children}
         </ThemeProvider>
       </body>
