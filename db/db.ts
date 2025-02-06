dd4b0c2815b920840c0e1ece9d914a872f964c98
dorik84 <dorya2005@mail.ru>
2025-02-06 11:52:31 -0400

    +mongoDB_DB

--- db/db.ts
+++ db/db.ts
@@ -1,10 +1,18 @@
-// src/db.ts
+"use server";
+
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGO_URI;
+const MONGODB_DB = process.env.MONGO_DB;
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGO_URI environment variable inside .env.local");
+}
+if (!MONGODB_DB) {
+  throw new Error("Please define the MONGO_DB environment variable inside .env.local");
 }
 
 let cached = global.mongoose;
@@ -23,8 +31,9 @@ async function dbConnect() {
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(MONGODB_URI, {
+      .connect(MONGODB_URI!, {
         bufferCommands: false,
+        dbName: MONGODB_DB,
       })
       .then((mongoose) => {
         console.log("Db connected");
