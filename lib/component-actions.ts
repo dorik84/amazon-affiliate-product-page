155f1f08ba4dcb2576789988fc08f8ee480e49f4
dorik84 <dorya2005@mail.ru>
2025-02-08 13:58:44 -0400

    attemp to add caching to getProduct

--- lib/component-actions.ts
+++ lib/component-actions.ts
@@ -1,11 +1,10 @@
-import { ProductData } from "@/types/productData";
+import { cache } from "react";
 import { sanitizeProductData } from "./utils";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export async function fetchCache(endpoint: string, options = {}) {
+export async function fetcher(endpoint: string, options = {}) {
   const response = await fetch(`${baseUrl}${endpoint}`, {
-    next: { revalidate: 6 },
     ...options,
   });
   if (!response.ok) {
@@ -14,27 +13,41 @@ export async function fetchCache(endpoint: string, options = {}) {
   return response.json();
 }
 
-export async function fetchNoCache(endpoint: string, options = {}) {
-  try {
-    const response = await fetch(`${baseUrl}${endpoint}`, { cache: "no-store", ...options });
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+const getProductEnclosure = () => {
+  let productPromise: Promise<any> | null = null;
 
-export async function getProduct(url: string) {
-  if (!url) return;
-  const productDB = await fetchCache(`/api/product?url=${encodeURIComponent(url)}`);
-  return sanitizeProductData(productDB);
-}
+  return (url: string) => {
+    if (!url) {
+      console.log("getProduct | no url provided");
+      return;
+    }
+
+    if (!productPromise) {
+      console.log("getProduct | fetching product data");
+      productPromise = fetcher(`/api/product?url=${encodeURIComponent(url)}`, {
+        next: { revalidate: 60 },
+      })
+        .then((productDB) => {
+          productPromise = null; // Reset the promise after it resolves
+          return sanitizeProductData(productDB);
+        })
+        .catch((error) => {
+          productPromise = null; // Reset the promise if there's an error
+          throw error;
+        });
+    } else {
+      console.log("getProduct | product data already fetching");
+    }
+
+    return productPromise;
+  };
+};
+
+export const getProduct = cache(getProductEnclosure());
 
 export async function getRelatedProducts() {
   try {
-    const relatedProducts = await fetchNoCache("/api/products");
+    const relatedProducts = await fetcher("/api/products", { cache: "no-store" });
 
     if (!relatedProducts) {
       console.log("No products found in DB");
