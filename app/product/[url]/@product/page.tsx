6094d506e27d638fccac23a66af33c35a2579624
dorik84 <dorya2005@mail.ru>
2025-02-14 11:53:19 -0400

    +ProductNotFound component

--- app/product/[url]/@product/page.tsx
+++ app/product/[url]/@product/page.tsx
@@ -1,12 +1,14 @@
 "use server";
 
+import ProductNotFound from "@/components/ProductNotFound";
 import ProductPage from "@/components/ProductPage";
 import { getProduct, updateProduct } from "@/lib/component-actions";
-import { ProductData } from "@/types/productData";
 
 export default async function Page({ params }: { params: { url: string } }) {
   // void updateProduct(params.url);
   let product = await getProduct(params.url);
-  console.log("url =", params.url);
-  return <ProductPage product={product as ProductData} />;
+  if (!product) {
+    return <ProductNotFound />;
+  }
+  return <ProductPage product={product} />;
 }
