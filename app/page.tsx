02826922bcd7b96c2542001906bbc9bfda2c150c
dorik84 <dorya2005@mail.ru>
2025-02-20 19:08:06 -0400

    +catch in RootPage for getProducts()

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,19 @@ import { Suspense } from "react";
 // import { getProducts } from "@/lib/server-actions";
 import { getProducts } from "@/lib/component-actions";
 
-export default async function Home() {
-  const response = await getProducts();
+export default async function RootPage() {
+  const response = await getProducts().catch((error) => {
+    console.error("[RootPage]:", error);
+    return null;
+  });
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
         <HeroSection />
         <Suspense fallback={<div>Loading...</div>}>
-          <PopularItems popularItems={response.data} />
+          <PopularItems popularItems={response?.data} />
         </Suspense>
       </main>
       <Footer />
