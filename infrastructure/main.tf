e71e31e3206e1c7acb6897f26ca68a8333b57471
dorik84 <dorya2005@mail.ru>
2025-04-02 22:33:14 -0300

    buntu_24_04->ubuntu_22_04

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -84,7 +84,7 @@ resource "aws_lightsail_instance" "next_app" {
   }
   name              = "next-app-instance"
   availability_zone = "us-east-2a"
-  blueprint_id      = "ubuntu_24_04"
+  blueprint_id      = "ubuntu_22_04"
   bundle_id         = "nano_2_0"
   key_pair_name     = "NextAppKey"
   user_data         = <<-EOF
