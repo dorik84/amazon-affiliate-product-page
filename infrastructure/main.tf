f0488614c8398027c531cd5d78b8151c630051b0
dorik84 <dorya2005@mail.ru>
2025-04-15 22:34:47 -0300

    fix namespace issue for metrics

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -85,7 +85,6 @@ variable "cw_aws_secret_access_key" {
   default     = ""
 }
 
-# Attach role to Lightsail instance (update existing resource)
 resource "aws_lightsail_instance" "next_app" {
   tags = { Name = "next-app-instance" }
   name              = "next-app-instance"
@@ -159,14 +158,14 @@ resource "aws_lightsail_instance" "next_app" {
     echo "[default]" > /root/.aws/config
     echo "region = us-east-2" >> /root/.aws/config
     echo '* * * * * root curl http://localhost:3000/api/health | aws cloudwatch put-metric-data --namespace "NextApp" --metric-name "HealthStatus" --value $([ $? -eq 0 ] && echo 1 || echo 0) --region us-east-2 >> /var/log/health-check.log 2>&1' > /etc/cron.d/health-check 2>&1 | tee -a /var/log/user-data.log
-    echo '* * * * * root free -m | grep Mem: | awk "{print (\\\$2-\\\\$7)/\\\\$2*100}" | xargs -I {} aws cloudwatch put-metric-data --namespace AWS/Lightsail --metric-name MemoryUtilization --value {} --region us-east-2 --dimensions InstanceName=next-app-instance >> /var/log/memory-check.log 2>&1' > /etc/cron.d/memory-check 2>&1 | tee -a /var/log/user-data.log
+    echo '* * * * * root free -m | grep Mem: | awk "{print (\\\$2-\\\\$7)/\\\\$2*100}" > /tmp/mem_usage && aws cloudwatch put-metric-data --namespace "AWS/Lightsail" --metric-name "MemoryUtilization" --value \$(cat /tmp/mem_usage) --region us-east-2 --dimensions InstanceName=next-app-instance >> /var/log/memory-check.log 2>&1' > /etc/cron.d/memory-check 2>&1 | tee -a /var/log/user-data.log
     sudo -u ubuntu docker-compose up -d 2>&1 | tee -a /var/log/user-data.log || echo "Docker Compose failed" >> /var/log/user-data.log
   EOF
 }
 
 resource "aws_iam_user_policy" "cloudwatch_metrics_policy" {
   name   = "CloudWatchMetricsPolicy"
-  user   = "cloudwatch"  # Adjust if your IAM user name differs
+  user   = "cloudwatch"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
